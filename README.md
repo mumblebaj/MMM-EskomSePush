689822bfb70b161f43c61c6c8b8dbe2b7e2de339
mumblebaj <bernardmumble@gmail.com>
2025-04-12 15:53:13 +0200

    add lint and prettier support

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ A [MagicMirror²](https://magicmirror.builders) module to display South African 
 
 [![Platform](https://img.shields.io/badge/platform-MagicMirror-informational)](https://MagicMirror.builders)
 [![license](https://img.shields.io/github/license/mashape/apistatus.svg)](LICENSE)
+[![ESLint Enabled](https://img.shields.io/badge/ESLint-Enabled-blue.svg)](https://eslint.org/)
+[![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat)](https://prettier.io/)
 
 ## 2.5hr Stint
 
@@ -22,6 +24,7 @@ A [MagicMirror²](https://magicmirror.builders) module to display South African 
 ![Example](images/image-4.png)
 
 ## Dependencies
+
 - Requires MagicMirror² v2.23.0
 - node-fetch v2.6.1
 - luxon v3.4.2
@@ -29,21 +32,25 @@ A [MagicMirror²](https://magicmirror.builders) module to display South African 
 ## Installation
 
 In your terminal, go to your MagicMirror's Module folder:
-````
+
+```
 cd ~/MagicMirror/modules
-````
+```
 
 Clone this repository:
-````
+
+```
 git clone https://github.com/mumblebaj/MMM-EskomSePush.git
-````
-````
+```
+
+```
 cd MMM-EskomSePush
 npm install
-````
+```
 
 Add the module to the modules array in the `config/config.js` file:
-````javascript
+
+```javascript
         {
             module: "MMM-EskomSePush",
             position: "middle_center", //Works best at middle_center. May not display all that well in other positions
@@ -55,50 +62,64 @@ Add the module to the modules array in the `config/config.js` file:
               fetchInterval: 30*60*1000
             }
 },
-````
+```
+
 ## Updates
 
 ### v1.2.8
+
 - On request have added a config to hide all elements when there is no Loadshedding scheduled.
-- New option `hideElements` have been added and can accept a value of `both`.  This will auto hide the module content when there is no loadshedding. This config can be removed to show the current `No Loadshedding`.
+- New option `hideElements` have been added and can accept a value of `both`. This will auto hide the module content when there is no loadshedding. This config can be removed to show the current `No Loadshedding`.
 
 ### v1.2.7
+
 - Updates to css to align better.
 - add MagicMirror Platform license
 
 ### V1.2.4
+
 - Updated module to show No upcoming loadshedding when there are no events for the scheduled stage.
 
 ### V1.2.2
+
 - Updates to remove invalid extra 30 minutes from display
 - With recent updates by City Power the current area code has been updated. Ensure latest area code is used if you live in the Johannesburg area.
 
 ## Registration for Token
+
 Go to [EskomSePush](https://eskomsepush.gumroad.com/l/api) and register for a free account. This allows you 50 calls per day. Obtain your token and determine your area as both these are required as input into the module.
 
 To Obtain your area you can run the collowing command from the command line:
 
-````bash
+```bash
 curl --request GET --url https://developer.sepush.co.za/business/2.0/areas_search?text=your-area-goes-here --header 'token: your-espsepush-token-here'
-````
+```
+
 - Replace `your-area-goes-here` with partial of your area
 - Replace `your-espsepush-token-here` with your ESP Token
 
 From the result set you can select the correct `id` for your area from the list.
 
-````json
-
-{"areas":[{"id":"jhbcitypower3-5-goldenharvestah","name":"Golden Harvest A.H (5)","region":"JHB City Power"},{"id":"jhbcitypower3-11-harveston","name":"Harveston (11)","region":"JHB City Power"}]}
-
-````
+```json
+{
+  "areas": [
+    {
+      "id": "jhbcitypower3-5-goldenharvestah",
+      "name": "Golden Harvest A.H (5)",
+      "region": "JHB City Power"
+    },
+    { "id": "jhbcitypower3-11-harveston", "name": "Harveston (11)", "region": "JHB City Power" }
+  ]
+}
+```
 
 ## Updating
 
 To update the module to the latest version, use your terminal to go to your MMM-EskomSePush module folder and type the following command:
 
-````
+```
 cd MMM-EskomSePush
 git pull
 npm install
 
-```` 
+```
--- node_helper.js
+++ node_helper.js
@@ -1,91 +1,90 @@
-var NodeHelper = require('node_helper')
-var fetch = require('node-fetch')
-var {
-    DateTime
-} = require('luxon')
+var NodeHelper = require("node_helper");
+var fetch = require("node-fetch");
+var { DateTime } = require("luxon");
 
 module.exports = NodeHelper.create({
-    requiresVersion: '2.23.0',
+  requiresVersion: "2.23.0",
 
-    start: function () {
-        console.log('Starting node_helper for module: ' + this.name)
+  start: function () {
+    console.log("Starting node_helper for module: " + this.name);
 
-        this.espUrl = "https://developer.sepush.co.za/business/2.0/area?id="
-    },
+    this.espUrl = "https://developer.sepush.co.za/business/2.0/area?id=";
+  },
 
-    deconstructData: function (data, code) {
-        var payload = data;
-        const espEvents = [];
-        if (payload.events.length >= 1) {
-            let eventsData = []
-            payload.events.forEach(event => {
-                let start1 = DateTime.fromISO(event.start);
-                let end1 = DateTime.fromISO(event.end);
-                const diff = end1.diff(start1, ["years", "months", "days", "hours"])
-                var timeDiff = diff.toObject()
-                eventsData.push({
-                    "stage": event.note,
-                    "relDate": DateTime.fromISO(event.start).toRelativeCalendar(),
-                    "start": event.start,
-                    "end": event.end,
-                    "startTime": DateTime.fromISO(event.start).hour + ":" + DateTime.fromISO(event.start).minute,
-                    "endTime": DateTime.fromISO(event.end).hour + ":" + DateTime.fromISO(event.end).minute,
-                    "duration": timeDiff.hours
-                })
-            });
-            espEvents.push({
-                "code": code,
-                "areaInfo": payload.info.name,
-                "region": payload.info.region,
-                "events": eventsData
-            })
-            return espEvents;
-        } else {
-            espEvents.push({
-                "code": code,
-                "areaInfo": payload.areaInfo,
-                "region": payload.region,
-                "events": "No upcoming loadshedding"
-            })
-            return espEvents;
-        }
-    },
-
-    async getEspData(payload) {
-        var tkn = payload.token
-        var endPoint = this.espUrl + payload.area
-
-        const response = await fetch(endPoint, {
-            method: 'get',
-            headers: {
-                'token': tkn
-            }
+  deconstructData: function (data, code) {
+    var payload = data;
+    const espEvents = [];
+    if (payload.events.length >= 1) {
+      let eventsData = [];
+      payload.events.forEach((event) => {
+        let start1 = DateTime.fromISO(event.start);
+        let end1 = DateTime.fromISO(event.end);
+        const diff = end1.diff(start1, ["years", "months", "days", "hours"]);
+        var timeDiff = diff.toObject();
+        eventsData.push({
+          stage: event.note,
+          relDate: DateTime.fromISO(event.start).toRelativeCalendar(),
+          start: event.start,
+          end: event.end,
+          startTime:
+            DateTime.fromISO(event.start).hour + ":" + DateTime.fromISO(event.start).minute,
+          endTime: DateTime.fromISO(event.end).hour + ":" + DateTime.fromISO(event.end).minute,
+          duration: timeDiff.hours
         });
-
-        if (response.status !== 200) {
-            const errorText = await response.text();
-            const espEvents = [];
-            const errorObject = JSON.parse(errorText);
-            const errorMessage = errorObject.error;
-            espEvents.push({
-                "code": response.status,
-                "areaInfo": payload.area,
-                "region": "",
-                "events": errorMessage
-            })
-            this.sendSocketNotification("ESP_DATA", espEvents)
-            return;
-        } else {
-            const data = await response.json();
-            const code = response.status;
-            var results = this.deconstructData(data, code);
-            this.sendSocketNotification("ESP_DATA", results)
-        }
-    },
-
-    socketNotificationReceived: function (notification, payload) {
-        if (notification === "GET_ESP_DATA") {
-            this.getEspData(payload)
-        }
+      });
+      espEvents.push({
+        code: code,
+        areaInfo: payload.info.name,
+        region: payload.info.region,
+        events: eventsData
+      });
+      return espEvents;
+    } else {
+      espEvents.push({
+        code: code,
+        areaInfo: payload.areaInfo,
+        region: payload.region,
+        events: "No upcoming loadshedding"
+      });
+      return espEvents;
     }
-})
+  },
+
+  async getEspData(payload) {
+    var tkn = payload.token;
+    var endPoint = this.espUrl + payload.area;
+
+    const response = await fetch(endPoint, {
+      method: "get",
+      headers: {
+        token: tkn
+      }
+    });
+
+    if (response.status !== 200) {
+      const errorText = await response.text();
+      const espEvents = [];
+      const errorObject = JSON.parse(errorText);
+      const errorMessage = errorObject.error;
+      espEvents.push({
+        code: response.status,
+        areaInfo: payload.area,
+        region: "",
+        events: errorMessage
+      });
+      this.sendSocketNotification("ESP_DATA", espEvents);
+      return;
+    } else {
+      const data = await response.json();
+      const code = response.status;
+      var results = this.deconstructData(data, code);
+      this.sendSocketNotification("ESP_DATA", results);
+    }
+  },
+
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === "GET_ESP_DATA") {
+      this.getEspData(payload);
+    }
+  }
+});
